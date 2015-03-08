571edb3ffe58ddf587e68f49d2bd5c267265555f
Hiram Cruz <hiram0101@gmail.com>
2015-03-08 16:12:52 -0400

    Rewrote ball and paddle classes to use vectors for velocity Started collision detection *still broken

--- main.py
+++ main.py
@@ -23,19 +23,44 @@ while 1:
     screen.fill(black)
     pygame.draw.line(screen, white, (width / 2, 0), (width / 2, height))
 
-    if pygame.key.get_pressed()[pygame.K_UP] != 0:
-        player_2.moveUp()
-    if pygame.key.get_pressed()[pygame.K_DOWN] != 0:
-        player_2.moveDown()
-    if pygame.key.get_pressed()[pygame.K_w] != 0:
-        player_1.moveUp()
-    if pygame.key.get_pressed()[pygame.K_s] != 0:
-        player_1.moveDown()
+    # Movement
+    player_2.moveUp(pygame.key.get_pressed()[pygame.K_UP])
+    player_2.moveDown(pygame.key.get_pressed()[pygame.K_DOWN])
 
+    player_1.moveUp(pygame.key.get_pressed()[pygame.K_w])
+    player_1.moveDown(pygame.key.get_pressed()[pygame.K_s])
+
+    # Display Paddles
     player_1.display()
     player_2.display()
 
-    #ball.update()
-    #ball.display()
+    # Y Collisions
+    if ball.posY + ball.radius >= height:
+        ball.collideY()
+    elif ball.posY - ball.radius <= 0:
+        ball.collideY()
+
+    # X Collisions
+    if ball.posX + ball.radius >= width - player_2.width:
+        # Right gutter check
+        if ball.posX + ball.radius >= width:
+            ball.reset()
+        # In-facing side of right paddle
+        if ball.posY >= player_2.posY and ball.posY <= player_2.posY + player_2.height:
+            ball.setVel(-(ball.vel[0] + player_2.vel[0]), ball.vel[1] + player_2.vel[1])
+        elif ball.posY - ball.radius <= player_2.posY + player_2.height:
+            ball.setVel(ball.vel[0] + player_2.vel[0], ball.vel[1] + player_2.vel[1])
+        elif ball.posY + ball.radius >= player_2.posY:
+            ball.setVel(ball.vel[0] + player_2.vel[0], ball.vel[1] + player_2.vel[1])
+    elif ball.posX - ball.radius <= player_1.width:
+        # Right gutter check
+        if ball.posX - ball.radius <= 0:
+            ball.reset()
+        # In-facing side of left paddle
+        if ball.posY >= player_1.posY and ball.posY <= player_1.posY + player_1.height:
+            ball.setVel(-(ball.vel[0] + player_2.vel[0]), ball.vel[1] + player_1.vel[1])
+
+    ball.update()
+    ball.display()
 
     pygame.display.flip()
--- objects.py
+++ objects.py
@@ -2,34 +2,40 @@ import pygame
 
 class Ball:
 
-    def __init__ (self, surface, color = (255, 255, 255), radius = 20, velX = 2, velY = 2):
+    def __init__ (self, surface, color = (255, 255, 255), radius = 15, velX = 2, velY = 2):
         self.surface = surface
         self.posX, self.posY = surface.get_width() / 2, surface.get_height() / 2
         self.color = (color)
         self.radius = radius
-        self.velX, self.velY = velX, velY
+        self.vel = [velX, velY]
 
     def display (self):
         pygame.draw.circle(self.surface, self.color, (self.posX, self.posY), self.radius)
 
     def update (self):
-        if self.posX + self.radius >= self.surface.get_width() or self.posX - self.radius <= 0:
-            self.velX *= -1
-        if self.posY + self.radius >= self.surface.get_height() or self.posY - self.radius <= 0:
-            self.velY *= -1
-        self.posX += self.velX
-        self.posY += self.velY
+        self.posX += self.vel[0]
+        self.posY += self.vel[1]
+
+    def collideY (self):
+        self.vel[1] *= -1
+
+    def setVel (self, x, y):
+        self.vel = [x, y]
+
+    def reset (self):
+        self.posX, self.posY = self.surface.get_width() / 2, self.surface.get_height() / 2
+        self.vel = [2, 2]
 
 class Paddle:
 
-    def __init__ (self, surface, side, color = (255, 255, 255), length_scale = .25):
+    def __init__ (self, surface, side, color = (255, 255, 255), height_scale = .25):
         self.surface = surface
         self.side = side
         self.color = color
-        self.length = self.surface.get_height() * length_scale
+        self.height = self.surface.get_height() * height_scale
         self.width = self.surface.get_width() / 40
-        self.vel = 4
-        self.posY = self.surface.get_height() / 2 - self.length / 2
+        self.vel = [0, 0]
+        self.posY = self.surface.get_height() / 2 - self.height / 2
         if side == 'left':
             self.posX = 0
         elif side == 'right':
@@ -38,12 +44,23 @@ class Paddle:
             print "ERROR: Paddle initialized incorrectly"
 
     def display (self):
-        pygame.draw.rect(self.surface, self.color, (self.posX, self.posY, self.width, self.length))
+        pygame.draw.rect(self.surface, self.color, (self.posX, self.posY, self.width, self.height))
 
-    def moveUp (self):
+    def update (self):
+        self.posY += self.vel[1]
+
+    def moveUp (self, pressed):
+        if pressed:
+            self.vel[1] = -4
+        else:
+            self.vel[1] = 0
         if not (self.posY <= 0):
-            self.posY -= self.vel
+            self.update()
 
-    def moveDown (self):
-        if not (self.posY >= self.surface.get_height() - self.length):
-            self.posY += self.vel
+    def moveDown (self, pressed):
+        if pressed:
+            self.vel[1] = 4
+        else:
+            self.vel[1] = 0
+        if not (self.posY >= self.surface.get_height() - self.height):
+            self.update()
