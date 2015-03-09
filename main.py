46078ff587fc36728c335b70e3e96918ee527537
Hiram Cruz <hiram0101@gmail.com>
2015-03-09 15:33:26 -0700

    fixed collision detection moved game initializations and loop into own class

--- main.py
+++ main.py
@@ -1,66 +1,7 @@
 import sys, pygame, objects
 
-pygame.init()
-pygame.key.set_repeat(0, 1)
 
-size = width, height = 640, 320
-screen = pygame.display.set_mode(size)
-black = (0, 0, 0)
-white = (255, 255, 255)
 
-clock = pygame.time.Clock()
-ball = objects.Ball(screen)
-player_1 = objects.Paddle(screen, 'left')
-player_2 = objects.Paddle(screen, 'right')
 
-while 1:
-    clock.tick_busy_loop(60)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
 
-    screen.fill(black)
-    pygame.draw.line(screen, white, (width / 2, 0), (width / 2, height))
-
-    # Movement
-    player_2.moveUp(pygame.key.get_pressed()[pygame.K_UP])
-    player_2.moveDown(pygame.key.get_pressed()[pygame.K_DOWN])
-
-    player_1.moveUp(pygame.key.get_pressed()[pygame.K_w])
-    player_1.moveDown(pygame.key.get_pressed()[pygame.K_s])
-
-    # Display Paddles
-    player_1.display()
-    player_2.display()
-
-    # Y Collisions
-    if ball.posY + ball.radius >= height:
-        ball.collideY()
-    elif ball.posY - ball.radius <= 0:
-        ball.collideY()
-
-    # X Collisions
-    if ball.posX + ball.radius >= width - player_2.width:
-        # Right gutter check
-        if ball.posX + ball.radius >= width:
-            ball.reset()
-        # In-facing side of right paddle
-        if ball.posY >= player_2.posY and ball.posY <= player_2.posY + player_2.height:
-            ball.setVel(-(ball.vel[0] + player_2.vel[0]), ball.vel[1] + player_2.vel[1])
-        elif ball.posY - ball.radius <= player_2.posY + player_2.height:
-            ball.setVel(ball.vel[0] + player_2.vel[0], ball.vel[1] + player_2.vel[1])
-        elif ball.posY + ball.radius >= player_2.posY:
-            ball.setVel(ball.vel[0] + player_2.vel[0], ball.vel[1] + player_2.vel[1])
-    elif ball.posX - ball.radius <= player_1.width:
-        # Right gutter check
-        if ball.posX - ball.radius <= 0:
-            ball.reset()
-        # In-facing side of left paddle
-        if ball.posY >= player_1.posY and ball.posY <= player_1.posY + player_1.height:
-            ball.setVel(-(ball.vel[0] + player_2.vel[0]), ball.vel[1] + player_1.vel[1])
-
-    ball.update()
-    ball.display()
-
-    pygame.display.flip()
--- objects.py
+++ objects.py
@@ -1,12 +1,12 @@
-import pygame
+import pygame, sys
 
 class Ball:
 
-    def __init__ (self, surface, color = (255, 255, 255), radius = 15, velX = 2, velY = 2):
+    def __init__ (self, surface, color = (255, 255, 255), velX = 2, velY = 2):
         self.surface = surface
         self.posX, self.posY = surface.get_width() / 2, surface.get_height() / 2
         self.color = (color)
-        self.radius = radius
+        self.radius = surface.get_width() / 40
         self.vel = [velX, velY]
 
     def display (self):
@@ -16,11 +16,14 @@ class Ball:
         self.posX += self.vel[0]
         self.posY += self.vel[1]
 
+    def collideX (self):
+        self.vel[0] *= -1
+
     def collideY (self):
         self.vel[1] *= -1
 
-    def setVel (self, x, y):
-        self.vel = [x, y]
+    def displace(self, amount):
+        self.posX += amount
 
     def reset (self):
         self.posX, self.posY = self.surface.get_width() / 2, self.surface.get_height() / 2
@@ -64,3 +67,71 @@ class Paddle:
             self.vel[1] = 0
         if not (self.posY >= self.surface.get_height() - self.height):
             self.update()
+
+    def reset (self):
+        self.posY = self.surface.get_height() / 2 - self.height / 2
+
+class Game:
+
+    def __init__(self, size):
+        pygame.init()
+        pygame.key.set_repeat(0, 1)
+        self.size = self.width, self.height = size
+        self.screen = pygame.display.set_mode(size)
+        self.black = (0, 0, 0)
+        self.white = (255, 255, 255)
+        self.clock = pygame.time.Clock()
+        self.ball = Ball(self.screen)
+        self.player_1 = Paddle(self.screen, 'left')
+        self.player_2 = Paddle(self.screen, 'right')
+
+    def newGame (self):
+        while 1:
+            self.clock.tick_busy_loop(60)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.black)
+            pygame.draw.line(self.screen, self.white, (self.width / 2, 0), (self.width / 2, self.height))
+
+            # Movement
+            self.player_2.moveUp(pygame.key.get_pressed()[pygame.K_UP])
+            self.player_2.moveDown(pygame.key.get_pressed()[pygame.K_DOWN])
+
+            self.player_1.moveUp(pygame.key.get_pressed()[pygame.K_w])
+            self.player_1.moveDown(pygame.key.get_pressed()[pygame.K_s])
+
+            # Display Paddles
+            self.player_1.display()
+            self.player_2.display()
+
+            # Y Collisions
+            if self.ball.posY + self.ball.radius >= self.height:
+                self.ball.collideY()
+            elif self.ball.posY - self.ball.radius <= 0:
+                self.ball.collideY()
+
+            # X Collisions
+            if self.width - self.player_2.width / 2 >= self.ball.posX + self.ball.radius >= self.width - self.player_2.width:
+                if self.ball.posY - self.ball.radius <= self.player_2.posY + self.player_2.height:
+                    if self.ball.posY + self.ball.radius >= self.player_2.posY:
+                        if self.width - self.player_2.width / 2 >= self.ball.posX + self.ball.radius > self.width - self.player_2.width:
+                            self.ball.displace(self.ball.posX - self.player_2.posX)
+                        self.ball.collideX()
+
+            elif self.player_1.width / 2 <= self.ball.posX - self.ball.radius <= self.player_1.width:
+                if self.ball.posY - self.ball.radius <= self.player_1.posY + self.player_1.height:
+                    if self.ball.posY + self.ball.radius >= self.player_1.posY:
+                        if self.player_1.width / 2 <= self.ball.posX - self.ball.radius < self.player_1.width:
+                            self.ball.displace(self.ball.posX - self.player_1.width)
+                        self.ball.collideX()
+            elif (self.ball.posX + self.ball.radius > self.width - self.player_2.width / 2) or (self.ball.posX - self.ball.radius < self.player_1.width / 2):
+                self.ball.reset()
+
+
+            self.ball.update()
+            self.ball.display()
+
+            pygame.display.flip()
